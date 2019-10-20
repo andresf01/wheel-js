15f5891b4412cae6ba2e3baeaafc88cd0dc56078
Andrés Martínez <andres.felipe.martinez@correounivalle.edu.co>
2019-10-19 23:52:02 -0500

    For Github Pages

---
+++ docs/service-worker.js
@@ -0,0 +1,43 @@
+var cacheName = 'trampometro-v0.1';
+
+var filesToCache = [
+    './',
+    './index.html',
+    './main.css',
+    './main.js',
+    './icons/icon144.png'
+]
+
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
